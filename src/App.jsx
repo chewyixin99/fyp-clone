dd97bfb89a865495d924ddc4a596bd943b9ace07
Chew Yi Xin <chewyixin1999@gmail.com>
2023-07-31 00:11:41 +0800

    configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Maps from "./pages/Maps";
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Home />} path="/" />
+      <Route element={<Maps />} path="/maps" />
     </Routes>
   );
 };
