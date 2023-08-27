a0dd3da04a14bec74718bce18d0bc83f7e9835df
GanJL <jianlin.gan.2020@scis.smu.edu.sg>
2023-08-28 00:52:07 +0800

    PF-75: Implemented bus stop functions and refactored bus chart functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,139 +7,122 @@ const Home = () => {
   const [busSpeed, setBusSpeed] = useState(35);
   const [totalDistance, setTotalDistance] = useState(3100);
   const [RDPPM, setRDPPM] = useState(0)
-  const [pause_state, setPauseState] = useState(false);
-  const [runState, setRunState] = useState(false);
-  const [busStopState, setBusStopState] = useState(false); // if bus is at bus stop, stop adding travel distance
-
+  const [triggerRun, setTriggerRun] = useState(false);
+  const [triggerPause, setTriggerPause] = useState(false); // if bus is at bus stop, stop adding travel distance
+  const [distanceTravelled, setDistanceTravelled] = useState(0); // if bus is at bus stop, stop adding travel distance
   const travelRef = useRef(null);
   const pause_btn = useRef(null);
   const run_btn = useRef(null);
   const stop_btn = useRef(null);
-  const bus_stop_1 = useRef(null);
-  const bus_stop_2 = useRef(null);
 
   const relative_factor = 20 // affects bus speed -> how many times faster than actual speed
   const update_rate = 1 // affects bar update -> smoothness of animation
   const route_bar_width = 1200 //  simulator route bar width in pixels
 
   var runRef = null;
-  var pause_for_stop_ref_1 // reference to the setInterval function for pausing at bus stops
-  var pause_for_stop_ref_2
 
+  const bus_stop_data = [
+    {
+      "bus_stop_id": "1",
+      "bus_stop_name": "Bus Stop 1",
+      "bus_stop_relative_distance": 200,
+      "bus_stop_duration": 1000
+    },
+    {
+      "bus_stop_id": "2",
+      "bus_stop_name": "Bus Stop 2",
+      "bus_stop_relative_distance": 1000,
+      "bus_stop_duration": 2000
+    }
+  ]
+
+  const formatDistance = (data) => {
+    return Number(parseFloat(data/totalDistance*100)).toFixed(2)
+  }
+
+  const running_function = () => {
+    runRef = setInterval(() => {
+      var relative_distance_travelled = Number(parseFloat(document.querySelector(".route-travelled").style.width.split('%')[0]).toFixed(2))
+      setDistanceTravelled(relative_distance_travelled)
+      var listOfBusStopDistance = [formatDistance(200),formatDistance(1000)]
+      if (listOfBusStopDistance.includes(relative_distance_travelled.toString())) {
+
+        var item = bus_stop_data.filter((item)=> {return relative_distance_travelled.toString() == formatDistance(item.bus_stop_relative_distance)})
+        var duration = item[0].bus_stop_duration
+        clearInterval(runRef)
+        setTimeout(()=>{
+          add_travel_distance(RDPPM)
+          running_function()
+        }, duration);
+      }
+      add_travel_distance(RDPPM)
+    }, 1);
+
+  }
 
   useEffect(() => {
-    if (runState) {
-      runRef = setInterval(() => {
-        if (parseFloat(document.querySelector(".route-travelled").style.width.split('%')[0]) > 100.0) {
-          clearInterval(runRef)
-        }
-        add_travel_distance(RDPPM)
+    loadBusStops()
+    if (triggerRun){
+      running_function()
+    }    
+    return () => clearInterval(runRef);
+  }, [triggerRun,triggerPause]);
 
-      }, update_rate);
-      return () => clearInterval(runRef);
+
+  const add_travel_distance = (relative_speed) => {
+    if (!triggerPause) {
+      travelRef.current.style.width = (parseFloat(travelRef.current.style.width.split('%')[0]) + relative_speed).toString() + "%";
     }
-    else {
-      clearInterval(runRef)
-    }
-  }, [runState, pause_state, busStopState]);
-
-  const pause_for_stop_1 = () => {
-
-    var actual_b1_distance = 600
-    var relative_b1_distance_percentage = actual_b1_distance / totalDistance * 100
-    var relative_b1_distance = route_bar_width * relative_b1_distance_percentage / 100
-    bus_stop_1.current.style.left = relative_b1_distance + "px";
-
-    pause_for_stop_ref_1 = setInterval(() => {
-
-      var route_bar_width = parseFloat(travelRef.current.style.width.split('%')[0]);
-      if (route_bar_width >= relative_b1_distance_percentage) {
-        setBusStopState(true)
-        setTimeout(() => {
-          setBusStopState(false);
-          clearInterval(pause_for_stop_ref_1);
-        }, 1000);
-      }
-
-    }, update_rate);
   }
 
-  const pause_for_stop_2 = () => {
-
-    var actual_b2_distance = 1000
-    var relative_b2_distance_percentage = actual_b2_distance / totalDistance * 100
-    var relative_b2_distance = route_bar_width * relative_b2_distance_percentage / 100
-    bus_stop_2.current.style.left = relative_b2_distance + "px";
-
-    pause_for_stop_ref_2 = setInterval(() => {
-
-      var route_bar_width = parseFloat(travelRef.current.style.width.split('%')[0]);
-      if (route_bar_width >= relative_b2_distance_percentage) {
-        setBusStopState(true)
-        setTimeout(() => {
-          setBusStopState(false)
-          clearInterval(pause_for_stop_ref_2);
-        }, 1000);
-      }
-
-    }, update_rate);
-  }
-
-  const add_travel_distance = (percentage_distance) => {
-
-    if (!pause_state && !busStopState) {
-      travelRef.current.style.width = (parseFloat(travelRef.current.style.width.split('%')[0]) + percentage_distance).toString() + "%";
+  const loadBusStops = () => {
+    var busStopHTML = ``
+    for (var i = 0; i < bus_stop_data.length; i++) {
+      var relative_distance_percentage = bus_stop_data[i].bus_stop_relative_distance / totalDistance * 100
+      var relative_distance = route_bar_width * relative_distance_percentage / 100
+      busStopHTML += `<div class="bus-stop-${i+1}" style="left:${relative_distance}px">&nbsp;</div>`
     }
+    document.querySelector(".bus-stop").innerHTML += busStopHTML
 
   }
 
-  const run = () => {
+  const startRun = () => {
 
-    pause_for_stop_1();
-    pause_for_stop_2();
-
-    // pause_btn.current.hidden = false;
     pause_btn.current.classList.remove('hidden');
     run_btn.current.classList.add('hidden');
-    // stop_btn.current.hidden = false;
     stop_btn.current.classList.remove('hidden');
-
+    // how much percentage of the journey the bus travels realistically per millisecond
     var actual_distance_percentage_per_millisecond = busSpeed / 3600 * 1000 / 1000 / totalDistance * 100;
+    // how much percentage of the journey the bus travels relatively per millisecond
     var relative_distance_percentage_per_millisecond = actual_distance_percentage_per_millisecond * relative_factor;
     setRDPPM(relative_distance_percentage_per_millisecond)
-
-    setRunState(true)
+    // loadBusStops()
+    setTriggerRun(true)
 
   }
 
   const pause = () => {
 
-    setPauseState(!pause_state)
-    if (pause_state) {
+    setTriggerPause(!triggerPause)
+    if (triggerPause) {
       pause_btn.current.innerHTML = 'Pause';
     }
     else {
       pause_btn.current.innerHTML = 'Unpause';
     }
-
   }
 
   const stop = () => {
 
-    setRunState(false)
-    setPauseState(false)
+    setTriggerRun(false)
+    setTriggerPause(false)
+    setDistanceTravelled(0)
     travelRef.current.style.width = "0%";
-    // pause_btn.current.hidden = false
-    // run_btn.current.hidden = false;
-    // stop_btn.current.hidden = true;
-
     pause_btn.current.classList.add('hidden');
     run_btn.current.classList.remove('hidden');
     stop_btn.current.classList.add('hidden');
 
   }
-
-
 
   return <div>
     <div className="container">
@@ -160,15 +143,15 @@ const Home = () => {
             </div>
 
             <div className="sm:flex">
-                <button onClick={run} id="run_simulation" ref={run_btn} type="button" className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                  Start
-                </button>
-                <button onClick={stop} id="stop_simulation" ref={stop_btn} type="button" className="hidden inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                  Stop
-                </button>
-                <button onClick={pause} id="pause_simulation" ref={pause_btn} type="button" className="hidden inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                  Pause
-                </button>
+              <button onClick={startRun} id="run_simulation" ref={run_btn} type="button" className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                Start
+              </button>
+              <button onClick={stop} id="stop_simulation" ref={stop_btn} type="button" className="hidden inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                Stop
+              </button>
+              <button onClick={pause} id="pause_simulation" ref={pause_btn} type="button" className="hidden inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                Pause
+              </button>
 
             </div>
           </div>
@@ -177,17 +160,9 @@ const Home = () => {
             <div className="route-travelled" style={{ width: '0%' }} ref={travelRef}>
               &nbsp;
             </div>
-            <div className="bus-stop-1" ref={bus_stop_1}>
-              &nbsp;
-            </div>
-            <div className="bus-stop-2" ref={bus_stop_2}>
-              &nbsp;
-            </div>
-            <div className="traffic-light-1">
-              &nbsp;
-            </div>
-
+            <div className="bus-stop"></div>
           </div>
+          <div>{distanceTravelled}</div>
         </div>
       </div>
 
