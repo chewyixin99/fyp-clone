5beb673d0a511b27c98efcecfb22884d7117bfc0
Kok Wee LOH <lohkokwee@gmail.com>
2023-09-13 01:59:26 +0800

    PF-91: update placeholder endpoint for reference for documentation

--- app/main.py
+++ app/main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI
 
 from .routers import admin
 
+tags_metadata = [
+  {
+    "name": "admin",
+    "description": "Administrative endpoints to query different statuses and stats of the server."
+  }
+]
+
 app = FastAPI(
   title="Star Command",
   summary="API endpoints to integrate the Visualizer, MM and Pippen's data.",
+  openapi_tags=tags_metadata,
   docs_url="/"
 )
 
-app.include_router(
-  admin.router,
-  prefix="/admin",
-  tags=["admin"],
-  responses={404: {"message": "Not found"}}
-)
+app.include_router(admin.router)
--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+  prefix="/admin",
+  responses={404: {"message": "Not found"}}
+)
 
-@router.get("healthcheck")
+@router.get(
+  "healthcheck",
+  description="Provides the status of the server.",
+  tags=["admin"]
+)
 def healthcheck():
   return {"data": "Status OK"}
