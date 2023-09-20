1c46d7fa0ecd972741a6e6e66d3ba5c5a7d0f9e2
Chew Yi Xin <chewyixin1999@gmail.com>
2023-09-20 17:06:40 +0800

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Journey from "./pages/Journeys";
-import MapsPage from "./pages/MapsPage";
 import CombinedPage from "./pages/CombinedPage";
 
 const App = () => {
@@ -9,8 +8,7 @@ const App = () => {
     <Routes>
       <Route element={<Home />} path="/">
         <Route element={<Journey />} index />
-        <Route element={<MapsPage />} path="maps" />
-        <Route element={<CombinedPage />} path="settings" />
+        <Route element={<CombinedPage />} path="combined" />
       </Route>
     </Routes>
   );
