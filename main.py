de611d4df345144b3cccc33f32fef435a37ded94
hellobiondi <biondi.lee.2020@scis.smu.edu.sg>
2023-10-01 10:47:30 +0800

    added actual.json, implemented visual coordinates check in main.py

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import json
 import re
+import plotly.express as px
+import pandas as pd
 from models.v1_4 import run_model # NOTE: to change to other models (not frequent)
 from utils.transformation import convert_json_to_dict, write_data_to_json, json_to_feed
+
+def check_coords(input_data):
+
+    coordinates_list = input_data["coordinates_list"]
+    df = pd.DataFrame(coordinates_list, columns=["Latitude", "Longitude"])
+
+    fig = px.line_mapbox(df, lat="Latitude", lon="Longitude", 
+                        mapbox_style="carto-positron", 
+                        zoom=10, 
+                        title="Bus route Plot")
+
+    fig.show()
 
 if __name__ == "__main__":
 
     model = "v1_4" # NOTE: to change to other models (not frequent)
-    polling_rate = 30
+    polling_rate = 1
     is_first_trip = False
     silent = False
 
     input_data = convert_json_to_dict("./data/inputs/actual/actual.json")
     # input_data = convert_json_to_dict("./data/inputs/mock/mock_input_2908.json")
+
+    # check_coords(input_data)
 
     try:
         if bool(re.match(r'v([3-9]|\d{2,})\_\d+|v2\_[0-9]+', model)): # check for models versions v2.0 and above
@@ -24,7 +40,7 @@ if __name__ == "__main__":
 
     try:
         write_data_to_json(
-        output_file_path=f"./data/outputs/json/{model}/{model}_output.json",
+        output_file_path=f"./data/outputs/json/{model}/{model}_optimised_output.json",
         num_trips=input_data["num_trips"],
         num_stops=input_data["num_stops"],
         bus_capacity=input_data["bus_capacity"],
@@ -38,6 +54,7 @@ if __name__ == "__main__":
         headway_matrix=output_data["headway_dict"],
         stranded_matrix=output_data["stranded_dict"],
         dispatch_list=output_data["dispatch_dict"],
+        objective_value=output_data["objective_value"],
         )
     except Exception as e:
         print("write_data_to_json failed")
@@ -45,8 +62,8 @@ if __name__ == "__main__":
 
     try:
         json_to_feed(
-            f"./data/outputs/json/{model}/{model}_output.json",
-            f"./data/outputs/csv/{model}/{model}_poll{polling_rate}_feed.csv",
+            f"./data/outputs/json/{model}/{model}_optimised_output.json",
+            f"./data/outputs/csv/{model}/{model}_poll{polling_rate}_optimised_feed.csv",
             polling_rate=polling_rate
         )
     except Exception as e:
