3a51aa6efaf7204337fef27761e0c35e17993c12
Chew Yi Xin <chewyixin1999@gmail.com>
2023-10-27 10:39:13 +0800

    PF-201: skeleton for feature

--- src/components/DispatchTimings.jsx
+++ src/components/DispatchTimings.jsx
@@ -3,28 +3,44 @@ import PropTypes from "prop-types";
 
 const DispatchTimings = React.memo(({ dispatchTimes }) => {
   return (
-    <div className="flex justify-center">
-      <div className="border-r-2 px-3 mx-3">
+    <div className="overflow-y-scroll h-[300px]">
+      <div className="px-3 mx-3 pb-3">
         Dispatch timings (sec) (mock values)
       </div>
       <div>
+        <div className="flex font-semibold">
+          <div className="text-center w-[50px] border">#</div>
+          <div className="text-center w-[100px] border">Planned</div>
+          <div className="text-center w-[100px] border">
+            <span>Actual</span>
+          </div>
+          <div className="text-center w-[100px] border">
+            <span>Δ</span>
+          </div>
+        </div>
+      </div>
+      <div>
         {Object.keys(dispatchTimes).map((trip) => {
-          const plannedTime = dispatchTimes[trip];
+          const plannedTime = parseInt(dispatchTimes[trip]);
           const newTime = parseInt(
-            dispatchTimes[trip] - 50 + Math.floor(Math.random() * 100)
+            dispatchTimes[trip] - 100 + Math.floor(Math.random() * 200)
           );
+          const diff = plannedTime - newTime;
           const textColor =
             newTime > plannedTime ? "text-orange-500" : "text-green-500";
           return (
             <div className="flex" key={trip}>
-              <div className="mr-3 pr-3 border-r-2">
-                Trip {parseInt(trip) + 1}
+              <div className="text-center w-[50px] border">
+                {parseInt(trip) + 1}
               </div>
-              <div className="mr-3 pr-3 border-r-2">
-                Planned = {plannedTime}
+              <div className="text-center w-[100px] border">
+                {plannedTime}
               </div>
-              <div className="mr-3 pr-3 border-r-2">
-                Actual = <span className={`${textColor}`}>{newTime}</span>
+              <div className="text-center w-[100px] border">
+                <span className={`${textColor}`}>{newTime}</span>
+              </div>
+              <div className="text-center w-[100px] border">
+                <span className={`${textColor}`}>{diff}</span>
               </div>
             </div>
           );
