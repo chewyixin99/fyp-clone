e8503c3a8964c3e1bd1d2a9b509f5340929b82c3
Chew Yi Xin <chewyixin1999@gmail.com>
2023-10-28 22:32:53 +0800

    PF-201: Integrated with backend to rerun model on user input

--- src/components/DispatchTimings.jsx
+++ src/components/DispatchTimings.jsx
@@ -3,12 +3,14 @@ import Papa from "papaparse";
 import { PuffLoader } from "react-spinners";
 import PropTypes from "prop-types";
 import { processCsvData } from "../util/mapHelper";
+import { TiTick } from "react-icons/ti";
 
 const DispatchTimings = React.memo(({ dispatchTimes }) => {
   const [dispatchInput, setDispatchInput] = useState({});
   const [loadingFetch, setLoadingFetch] = useState(false);
   const [errorFetch, setErrorFetch] = useState(false);
   const [errorMsgFetch, setErrorMsgFetch] = useState("");
+  const [updatedData, setUpdatedData] = useState({});
 
   const handleInputChange = (e) => {
     const trip = e.target.id;
@@ -41,7 +43,7 @@ const DispatchTimings = React.memo(({ dispatchTimes }) => {
     setLoadingFetch(true);
     setErrorFetch(false);
     setErrorMsgFetch("");
-    const url = "http://127.0.0.1:8000/mm/result_feed_stream";
+    const url = "http://127.0.0.1:8000/mm/result_feed";
     const requestBody = {
       unoptimised: false,
       polling_rate: 1,
@@ -56,55 +58,17 @@ const DispatchTimings = React.memo(({ dispatchTimes }) => {
       body: JSON.stringify(requestBody),
     };
 
-    // await fetch(url, options)
-    //   .then((response) => {
-    //     if (response.ok) {
-    //       setLoadingFetch(false);
-    //       return response.text();
-    //     }
-    //   })
-    //   .then((csvData) => {
-    //     const parsed = Papa.parse(csvData).data.slice(1);
-    //     const processedData = processCsvData(parsed);
-    //     // continue
-    //   })
-    //   .catch((e) => {
-    //     setLoadingFetch(false);
-    //     setErrorFetch(true);
-    //     console.log(e);
-    //     setErrorMsgFetch(e.message);
-    //   });
-
     await fetch(url, options)
-      .then(response => {
-        const reader = response.body.getReader(); // Create a ReadableStream from the response body
-        let partialData = ''; // Store the remaining partial data
-      
-        // Define a function to process each data chunk
-        const processData = ({ done, value }) => {
-          const chunk = value ? partialData + new TextDecoder().decode(value) : partialData; // Combine the partial data with the new chunk - stream chunks may come in multiple at a time
-          const jsonObjects = chunk.split('\n').filter(Boolean);  // Split the chunk into individual JSON objects
-      
-          jsonObjects.forEach(jsonObject => { // Process each JSON object
-            try {
-              const data = JSON.parse(jsonObject);
-              console.log(data); // Process the data - check your logs YX
-            } catch(e) {
-              console.error('Error parsing JSON object:', e); // Hard to handle - probably can just ignore
-            }
-          });
-          
-          partialData = chunk.endsWith('\n') ? '' : jsonObjects[jsonObjects.length - 1] || ''; // Store any remaining partial data
-          
-          if (!done) { // Continue reading the next chunk of data
-            reader.read().then(processData); 
-          }
+      .then((response) => {
+        if (response.ok) {
+          setLoadingFetch(false);
+          return response.text();
         }
-      
-        reader.read().then(processData); // Read the first chunk of data
       })
-      .then(() => {
-        setLoadingFetch(false);
+      .then((csvData) => {
+        const parsed = Papa.parse(csvData).data.slice(1);
+        const processedData = processCsvData(parsed);
+        setUpdatedData(processedData);
       })
       .catch((e) => {
         setLoadingFetch(false);
@@ -112,16 +76,67 @@ const DispatchTimings = React.memo(({ dispatchTimes }) => {
         console.log(e);
         setErrorMsgFetch(e.message);
       });
+
+    // for fetching readable stream
+    //   await fetch(url, options)
+    //     .then(response => {
+    //       const reader = response.body.getReader(); // Create a ReadableStream from the response body
+    //       let partialData = ''; // Store the remaining partial data
+
+    //       // Define a function to process each data chunk
+    //       const processData = ({ done, value }) => {
+    //         const chunk = value ? partialData + new TextDecoder().decode(value) : partialData; // Combine the partial data with the new chunk - stream chunks may come in multiple at a time
+    //         const jsonObjects = chunk.split('\n').filter(Boolean);  // Split the chunk into individual JSON objects
+
+    //         jsonObjects.forEach(jsonObject => { // Process each JSON object
+    //           try {
+    //             const data = JSON.parse(jsonObject);
+    //             console.log(data); // Process the data - check your logs YX
+    //           } catch(e) {
+    //             console.error('Error parsing JSON object:', e); // Hard to handle - probably can just ignore
+    //           }
+    //         });
+
+    //         partialData = chunk.endsWith('\n') ? '' : jsonObjects[jsonObjects.length - 1] || ''; // Store any remaining partial data
+
+    //         if (!done) { // Continue reading the next chunk of data
+    //           reader.read().then(processData);
+    //         }
+    //       }
+
+    //       reader.read().then(processData); // Read the first chunk of data
+    //     })
+    //     .then(() => {
+    //       setLoadingFetch(false);
+    //     })
+    //     .catch((e) => {
+    //       setLoadingFetch(false);
+    //       setErrorFetch(true);
+    //       console.log(e);
+    //       setErrorMsgFetch(e.message);
+    //     });
   };
 
   const renderFetchButton = () => {
     return (
       <div className="my-3 flex justify-end mr-3 items-center">
-        <div>{errorMsgFetch}</div>
-        <button onClick={handleSubmit} className="control-button">
-          run model
+        <div className="text-orange-500">{errorMsgFetch}</div>
+        <button
+          onClick={handleSubmit}
+          className={`${
+            loadingFetch ? "control-button-disabled" : "control-button"
+          }`}
+        >
+          {loadingFetch ? "running model" : "update timings"}
         </button>
         <PuffLoader color="rgb(234, 88, 12)" loading={loadingFetch} size={15} />
+        {!loadingFetch &&
+        !errorFetch &&
+        Object.keys(updatedData).length !== 0 ? (
+          <TiTick className="text-green-500" />
+        ) : (
+          ""
+        )}
       </div>
     );
   };
